ce28c053792158fd02c0be35351c13de3a76f9ff
kumar827009 <kumar.naresh.bscity@gmail.com>
2018-10-24 18:11:29 +0530

    Test pull

---
+++ notebooks/Users/naresh.kumar@databricks.com/salmonTest-18013.py
@@ -0,0 +1,21 @@
+# Databricks notebook source
+# MAGIC %sh
+# MAGIC sudo apt-get update
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC sudo apt-get install salmon
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC salmon --help
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC 1+1
+
+# COMMAND ----------
+
